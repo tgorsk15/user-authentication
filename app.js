4c6eab9bb06cfe0943e1e0a32c3cdd61c45322b1
tgorsk15 <tgbusiness463@gmail.com>
2024-08-19 08:49:06 -0500

    Set up view templates, add dependencies to app.js

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
+///// app.js
+const { Pool }= require("pg");
 const express = require('express');
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require('passport-local').Strategy
+
+
+//// pool.js
+const pool = new Pool({
+    host: "localhost", // or wherever the db is hosted
+    user: "tyler15",
+    database: "users",
+    password: "Kfkenny13!!",
+    port: 5432 // the default port
+})
+
 const app = express();
-// import routers here
-
+app.set("views", __dirname);
 app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: true }))
+app.use(session({ secret: "cats", resave: false, saveUninitialized: false}))
+app.use(passport.session());
 
-// use routers here
+app.get("/", (req, res) => res.render("index"))
+app.get("/sign-up", (req, res) => res.render("sign-up"))
 
-const PORT = process.env.PORT || 5000
+app.use(express.urlencoded({ extended: false }))
+
+
+const PORT = process.env.PORT || 6000
 app.listen(PORT, () => console.log(`always watchin you on ${PORT}`))
+
