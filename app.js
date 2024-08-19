de644bb4269176f34c1d5c7782c297f88375fd85
tgorsk15 <tgbusiness463@gmail.com>
2024-08-19 10:46:43 -0500

    Add passport functions for strategy, serialize, deserialize

--- app.js
+++ app.js
@@ -38,6 +38,48 @@ app.post("/sign-up", async (req, res, next) => {
     }
 })
 
+// set up local strategy
+passport.use(
+    new LocalStrategy(async (username, password, done) => {
+        try {
+            const { rows } = await pool.query(`
+                SELECT * FROM users
+                WHERE username = $1
+            `, [username])
+            const user = rows[0];
+
+            if (!user) {
+                return done(null, false, { message: "incorrect username" })
+            }
+            if (user.password !== password) {
+                return done(null, false, { message: "Incorrect password" })
+            }
+            return done(null, user)
+
+        } catch(err) {
+            return done(err)
+        }
+    })
+)
+
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+})
+
+passport.deserializeUser(async (id, done) => {
+    try {
+        const { rows } = await pool.query(`
+            SELECT * FROM users
+            WHERE id = $1
+        `, [id]);
+        const user = rows[0];
+
+        done(null, user)
+    } catch(err) {
+        done(err)
+    }
+})
+
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`always watchin you on ${PORT}`))
